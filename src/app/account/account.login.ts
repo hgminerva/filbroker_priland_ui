7b195a4e43ddde79d65f7bf95b33942c7c26dff1
Noah Oliver Giron Rigonan <oliverrigonan@gmail.com>
2018-02-01 17:11:02 +0800

    User Authentication (User Registration)

--- src/app/account/account.login.ts
+++ src/app/account/account.login.ts
@@ -1,4 +1,4 @@
-import { Component,ViewContainerRef } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
 import { User } from './account.user';
 import { AccountService } from './account.service';
 import { Router } from '@angular/router';
@@ -9,27 +9,29 @@ import { ToastsManager } from 'ng2-toastr/ng2-toastr';
   styleUrls: ['./account.style.css'],
 })
 export class AccountLogin {
-  private loginSub : any;
+  private loginSub: any;
 
-  public title : string = 'Account Login';
-  public login : string;
+  public title: string = 'Account Login';
+  public login: string;
 
   public user: User = {
+    fullname: "",
     username: "",
     password: "",
+    confirmPassword: "",
     token: ""
   };
 
   constructor(
     private accountService: AccountService,
     private router: Router,
-    private toastr : ToastsManager,
-    private viewContainer : ViewContainerRef) {
-      this.toastr.setRootViewContainerRef(viewContainer);
+    private toastr: ToastsManager,
+    private viewContainer: ViewContainerRef) {
+    this.toastr.setRootViewContainerRef(viewContainer);
   }
 
   ngOnInit() {
-    if(localStorage.getItem("username")==null) {
+    if (localStorage.getItem("username") == null) {
       this.login = "Login";
     } else {
       this.router.navigate(['/menu']);
@@ -37,28 +39,28 @@ export class AccountLogin {
   }
 
   ngOnDestroy() {
-    if( this.loginSub != null) this.loginSub.unsubscribe();
+    if (this.loginSub != null) this.loginSub.unsubscribe();
   }
 
   public btnLoginClick(): void {
-    this.accountService.login(this.user.username,this.user.password);    
+    this.accountService.login(this.user.username, this.user.password);
     this.loginSub = this.accountService.loginObservable.subscribe(
-        data => {
-            if(data == 1) {
-              this.toastr.success("Login successful.");   
-              setTimeout(() => {
-                this.router.navigate(['/menu']);
-              }, 1000);
-            } else if(data == 0) {
-              this.toastr.error("Login failed.");   
-            }
+      data => {
+        if (data == 1) {
+          this.toastr.success("Login successful.");
+          setTimeout(() => {
+            this.router.navigate(['/menu']);
+          }, 1000);
+        } else if (data == 0) {
+          this.toastr.error("Login failed.");
         }
+      }
     );
 
   }
 
-  public loginClick() : void {
-    if(localStorage.getItem("username")==null) {
+  public loginClick(): void {
+    if (localStorage.getItem("username") == null) {
       this.router.navigate(['/account/login']);
     } else {
       localStorage.removeItem('access_token');
--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -8,29 +8,54 @@ import { Observable } from 'rxjs/Observable';
 
 import { User } from './account.user';
 
+// message box
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { parseHttpResponse } from "selenium-webdriver/http";
+
+import { ObservableArray } from 'wijmo/wijmo';
+
 @Injectable()
 export class AccountService {
+    // ==================
+    // private properties
+    // ==================
+    private headers = new Headers({
+        'Content-Type': 'application/json'
+    });
+    private options = new RequestOptions({ headers: this.headers });
+
+    // =================
+    // public properties
+    // =================
     public loginSource = new Subject<number>();
-    public loginObservable = this.loginSource.asObservable();   
+    public loginObservable = this.loginSource.asObservable();
+
+    public registerSource = new Subject<number>();
+    public registerObservable = this.registerSource.asObservable();
+
+    public registerErrorMessageSource = new Subject<ObservableArray>();
+    public registerErrorMessageObservable = this.registerErrorMessageSource.asObservable();
 
     public user: User = {
+        fullname: "",
         username: "",
         password: "",
+        confirmPassword: "",
         token: ""
-      };
+    };
 
-    constructor ( 
-        private router: Router, 
-        private http: Http) {
+    constructor(
+        private router: Router,
+        private http: Http,
+        private toastr: ToastsManager) {
     }
 
-    public login(username: string, password: string): void 
-    {
+    public login(username: string, password: string): void {
         let url = 'https://filbrokerwebsite-priland.azurewebsites.net/token';
         let body = "username=" + username + "&password=" + password + "&grant_type=password";
         let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
         let options = new RequestOptions({ headers: headers })
-    
+
         this.http.post(url, body, options).subscribe(
             response => {
                 localStorage.setItem('access_token', response.json().access_token);
@@ -42,6 +67,108 @@ export class AccountService {
             },
             error => {
                 this.loginSource.next(0);
+            }
+        )
+    }
+
+    public register(user: User): void {
+        let regObj = {
+            FullName: user.fullname,
+            UserName: user.username,
+            Email: user.username,
+            Password: user.password,
+            ConfirmPassword: user.confirmPassword
+        }
+
+        var registerMessageObservableArray = new ObservableArray();
+        var successMessageArray = new ObservableArray();
+        var mainErrorMessageArray = new ObservableArray();
+        var fullnameErrorMessageArray = new ObservableArray();
+        var usernameErrorMessageArray = new ObservableArray();
+        var passwordErrorMessageArray = new ObservableArray();
+        var confirmPasswordErrorMessageArray = new ObservableArray();
+
+        let url = "http://localhost:10136/api/account/Register";
+        this.http.post(url, JSON.stringify(regObj), this.options).subscribe(
+            response => {
+                if (response != null) {
+                    successMessageArray.push({
+                        message: "Register Successful"
+                    });
+
+                    registerMessageObservableArray.push({
+                        successMessageArray: successMessageArray,
+                        mainErrorMessageArray: null,
+                        fullnameErrorMessageArray: null,
+                        usernameErrorMessageArray: null,
+                        passwordErrorMessageArray: null,
+                        confirmPasswordErrorMessageArray: null
+                    });
+
+                    this.registerErrorMessageSource.next(registerMessageObservableArray);
+                    this.registerSource.next(1);
+                }
+            },
+            error => {
+                var errMessage = error.json();
+                if (errMessage != null) {
+                    if (error.json().ModelState[""] != null) {
+                        let mainErrorMessageLength = error.json().ModelState[""].length;
+                        for (var i = 0; i < mainErrorMessageLength; i++) {
+                            mainErrorMessageArray.push({
+                                message: error.json().ModelState[""][i]
+                            });
+                        }
+                    }
+
+                    if (error.json().ModelState["model.FullName"] != null) {
+                        let fullnameErrorMessageLength = error.json().ModelState["model.FullName"].length;
+                        for (var i = 0; i < fullnameErrorMessageLength; i++) {
+                            fullnameErrorMessageArray.push({
+                                message: error.json().ModelState["model.FullName"][i]
+                            });
+                        }
+                    }
+
+                    if (error.json().ModelState["model.UserName"] != null) {
+                        let usernameErrorMessageLength = error.json().ModelState["model.UserName"].length;
+                        for (var i = 0; i < usernameErrorMessageLength; i++) {
+                            usernameErrorMessageArray.push({
+                                message: error.json().ModelState["model.UserName"][i]
+                            });
+                        }
+                    }
+
+                    if (error.json().ModelState["model.Password"] != null) {
+                        let passwordErrorMessageLength = error.json().ModelState["model.Password"].length;
+                        for (var i = 0; i < passwordErrorMessageLength; i++) {
+                            passwordErrorMessageArray.push({
+                                message: error.json().ModelState["model.Password"][i]
+                            });
+                        }
+                    }
+
+                    if (error.json().ModelState["model.ConfirmPassword"] != null) {
+                        let confirmPasswordErrorMessageLength = error.json().ModelState["model.ConfirmPassword"].length;
+                        for (var i = 0; i < confirmPasswordErrorMessageLength; i++) {
+                            confirmPasswordErrorMessageArray.push({
+                                message: error.json().ModelState["model.ConfirmPassword"][i]
+                            });
+                        }
+                    }
+
+                    registerMessageObservableArray.push({
+                        successMessageArray: null,
+                        mainErrorMessageArray: mainErrorMessageArray,
+                        fullnameErrorMessageArray: fullnameErrorMessageArray,
+                        usernameErrorMessageArray: usernameErrorMessageArray,
+                        passwordErrorMessageArray: passwordErrorMessageArray,
+                        confirmPasswordErrorMessageArray: confirmPasswordErrorMessageArray
+                    });
+
+                    this.registerErrorMessageSource.next(registerMessageObservableArray);
+                    this.registerSource.next(0);
+                }
             }
         )
     }
